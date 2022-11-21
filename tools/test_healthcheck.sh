d931bcf7ad6eea15f43d9708c241ff38a2f6848f
lemaitre-aneo <flemaitre@aneo.fr>
2022-11-21 22:38:21 +0100

    Try fix healcheck test

--- tools/test_healthcheck.sh
+++ tools/test_healthcheck.sh
@@ -8,37 +8,49 @@ fi
 
 SERVICE=$1
 
-curl -fsSL localhost:5011/liveness
-curl -fsSL localhost:5011/startup
+healthcheck() {
+  curl -fsSL localhost:5011/liveness
+  curl -fsSL localhost:5011/startup
+  
+  curl -fsSL localhost:9980/liveness
+  curl -fsSL localhost:9980/startup
+  curl -fsSL localhost:9980/readiness
+}
 
-curl -fsSL localhost:9980/liveness
-curl -fsSL localhost:9980/startup
-curl -fsSL localhost:9980/readiness
+healthcheck
 echo
 
 docker-compose -f docker-compose/docker-compose.yml -f docker-compose/docker-compose.queue-activemqp.yml stop $SERVICE
 
 for i in $(seq 0 2);
 do
-  curl -sSL localhost:5011/liveness || true
-  curl -sSL localhost:9980/liveness || true
+  healthcheck || true
   echo
   sleep 1
 done
 
 docker-compose -f docker-compose/docker-compose.yml -f docker-compose/docker-compose.queue-activemqp.yml restart $SERVICE
 
+healthcheck || true
+sleep 1
 
-for i in $(seq 0 2);
-do
-  curl -sSL localhost:5011/liveness || true
-  curl -sSL localhost:5011/startup || true
-  curl -sSL localhost:9980/liveness || true
-  curl -sSL localhost:9980/startup || true
-  curl -sSL localhost:9980/readiness || true
-  echo
-  sleep 2
-done
+# If we restart the queue, wait for it to be active again
+if [ "$SERVICE" = queue ];
+then
+  for i in $(seq 5);
+  do
+    if curl -f -u admin:admin -s http://localhost:8161/api/jolokia/exec/org.apache.activemq:type=Broker,brokerName=localhost,service=Health/healthStatus -H Origin:http://localhost |
+      grep Good &>/dev/null;
+    then
+      break
+    fi
+    sleep 2
+  done
+fi
+
+healthcheck || true
+
+sleep 1
 
 docker-compose -f docker-compose/docker-compose.yml -f docker-compose/docker-compose.queue-activemqp.yml restart armonik.control.submitter
 docker-compose -f docker-compose/docker-compose.yml -f docker-compose/docker-compose.queue-activemqp.yml restart armonik.compute.pollingagent0
@@ -47,19 +59,10 @@ docker-compose -f docker-compose/docker-compose.yml -f docker-compose/docker-com
 
 for i in $(seq 0 2);
 do
-  sleep 2
-  curl -sSL localhost:5011/liveness || true
-  curl -sSL localhost:5011/startup || true
-  curl -sSL localhost:9980/liveness || true
-  curl -sSL localhost:9980/startup || true
-  curl -sSL localhost:9980/readiness || true
+  sleep 1
+  healthcheck || true
   echo
 done
 
-curl -fsSL localhost:5011/liveness
-curl -fsSL localhost:5011/startup
-
-curl -fsSL localhost:9980/liveness
-curl -fsSL localhost:9980/startup
-curl -fsSL localhost:9980/readiness
+healthcheck
 echo
